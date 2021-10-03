1df667c51b9c53b7ccc04751cf094a63320d8579
Daniil Ekzarian <reflash-ne@yandex.ru>
2021-10-03 22:30:06 +0000

    addTask impl

--- src/endpoints/botWebhook.ts
+++ src/endpoints/botWebhook.ts
@@ -3,10 +3,15 @@ import { bot } from '../service/bot';
 
 exports.handler = handlerAdapter(async ({ req }) => {
     if (!req) return notFound('No request object');
+    try {
+        await bot.init();
+        const update = req.body;
+        await bot.handleUpdate(update);
+    } catch(e) {
+        const message = `Request: ${JSON.stringify(req)}\nError: ${JSON.stringify(e)}`;
+        await bot.api.sendMessage(process.env.USER_ID!, message);
+    }
     
-    await bot.init();
-    const update = req.body;
-    await bot.handleUpdate(update);
 
     return success('Message processed');
 });
