2cdaa0facc7e77a9c1e95f3b2ff0b91d0951948d
Daniil Ekzarian <reflash-ne@yandex.ru>
2021-10-04 12:02:15 +0000

    fixed bot webhook

--- src/endpoints/botWebhook.ts
+++ src/endpoints/botWebhook.ts
@@ -1,5 +1,7 @@
 import { handlerAdapter, notFound, success } from '../utils/azure';
 import { bot } from '../service/bot';
+import { stringToReadable } from '../utils';
+import { InputFile } from 'grammy';
 
 exports.handler = handlerAdapter(async ({ req }) => {
     if (!req) return notFound('No request object');
@@ -9,10 +11,8 @@ exports.handler = handlerAdapter(async ({ req }) => {
         await bot.handleUpdate(update);
     } catch(e) {
         const message = `Request: ${JSON.stringify(req)}\nError: ${JSON.stringify(e, Object.getOwnPropertyNames(e))}`;
-        var s = new Readable()
-        s.push(message);
-        s.push(null);
-        await bot.api.sendDocument(process.env.USER_ID!, message);
+        const stream = stringToReadable(message);
+        await bot.api.sendDocument(process.env.USER_ID!, new InputFile(stream));
     }
     
 
